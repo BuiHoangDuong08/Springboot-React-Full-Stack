6e8764f357b70e25135f90ce15df35d0f31c69d4
BuiHoangDuong08 <buihoangduong.dev@gmail.com>
2025-08-17 20:54:18 +0700

    fix-port-backend

--- Springboot React full stack/studentsytem/src/main/java/com/hoangduongcode/studentsytem/controller/StudentController.java
+++ Springboot React full stack/studentsytem/src/main/java/com/hoangduongcode/studentsytem/controller/StudentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class StudentController {
     @Autowired
     private StudentService studentService;
